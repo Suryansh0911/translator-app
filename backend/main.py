8b2d5ffb9ee53a8cf411e166d1a80b244b0ead71
SHADOW <suryanshgupta0911@gmail.com>
2025-09-05 14:34:27 +0530

    allowed all origins

--- backend/main.py
+++ backend/main.py
@@ -47,7 +47,7 @@ app = FastAPI(title="Translator+TTS")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "https://translator-app-theta-rust.vercel.app/"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
